88b8f4d0277275baf41835c1aa7ad51b4005e178
Harihareswaran <harihareswaranthirumalai@gmail.com>
2026-02-14 14:54:56 +0530

    added json

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
     features = "src/test/resources/features",
     glue = {"StepDefinition","hooks"},
-    plugin = {"pretty","html:target/cucumber-reports.html"},
+    plugin = {"pretty","html:target/cucumber-reports.html","json:target/cucumber-report.json"},       // JSON for Jenkins
     monochrome = true,
     tags = "@smoke or @regression"  // Runs ALL scenarios with any of these tags
 )
